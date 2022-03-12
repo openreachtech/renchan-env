92372321a5c19ee96ac9a6ff4d5038f4bf6a838f
Stew Eucen <stew.eucen@gmail.com>
2022-03-12 18:49:30 +0900

    Add test to DotenvLoader#resolveDotenvPath().

---
+++ tests/__tests__/lib/DotenvLoader.js
@@ -0,0 +1,43 @@
+// @ts-check
+'use strict'
+
+const DotenvLoader = require('../../../lib/DotenvLoader')
+
+describe('DotenvLoader', () => {
+  describe('#resolveDotenvPath()', () => {
+    describe('throw exception', () => {
+      const cases = [
+        null,
+        undefined,
+        '',
+      ]
+
+      test.each(cases)('%s', (nodeEnv) => {
+        const env = new DotenvLoader(nodeEnv)
+
+        expect(() => env.resolveDotenvPath())
+          .toThrowError('no NODE_ENV')
+      })
+    })
+
+    describe('with NODE_ENV', () => {
+      const cases = [
+        ['development', '/.env.development'],
+        ['staging', '/.env.staging'],
+        ['extra', '/.env.extra'],
+      ]
+
+      test.each(cases)('%s', (nodeEnv, fileName) => {
+        const env = new DotenvLoader(nodeEnv)
+
+        expect(env.resolveDotenvPath().endsWith(fileName)).toBeTruthy()
+      })
+
+      test('NODE_ENV: production', () => {
+        const env = new DotenvLoader('production')
+
+        expect(env.resolveDotenvPath()).toBeNull()
+      })
+    })
+  })
+})
