27e473698638be3eeb410c43528014954dca1712
Stew Eucen <stew.eucen@gmail.com>
2022-03-12 18:55:30 +0900

    Add test to DotenvLoader#createDotenvOptions().

--- tests/__tests__/lib/DotenvLoader.js
+++ tests/__tests__/lib/DotenvLoader.js
@@ -40,4 +40,41 @@ describe('DotenvLoader', () => {
       })
     })
   })
+
+  describe('#createDotenvOptions()', () => {
+    describe('throw exception', () => {
+      const cases = [
+        null,
+        undefined,
+        '',
+      ]
+
+      test.each(cases)('%s', (nodeEnv) => {
+        const env = new DotenvLoader(nodeEnv)
+
+        expect(() => env.createDotenvOptions())
+          .toThrowError('no NODE_ENV')
+      })
+    })
+
+    describe('with NODE_ENV', () => {
+      const cases = [
+        ['development', '/.env.development'],
+        ['staging', '/.env.staging'],
+        ['extra', '/.env.extra'],
+      ]
+
+      test.each(cases)('%s', (nodeEnv, fileName) => {
+        const env = new DotenvLoader(nodeEnv)
+
+        expect(env.createDotenvOptions().path.endsWith(fileName)).toBeTruthy()
+      })
+
+      test('NODE_ENV: production', () => {
+        const env = new DotenvLoader('production')
+
+        expect(env.createDotenvOptions()).toEqual({})
+      })
+    })
+  })
 })
