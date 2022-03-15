99cd13638efc7f5b95adf96be010ceaec73303d3
Stew Eucen <stew.eucen@gmail.com>
2022-03-15 15:51:09 +0900

    Add test to typedEnv with Environment.createEnv().

---
+++ tests/app/typedEnv.js
@@ -0,0 +1,11 @@
+// @ts-check
+'use strict'
+
+const Environment = require('../../lib/Environment')
+
+module.exports = /** @type {{
+  NODE_ENV: string,
+
+  API_HOST?: string,
+  API_KEY?: string,
+}} */ (Environment.createEnv())
