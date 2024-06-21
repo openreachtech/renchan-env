10e4895e6858ae0398e6ef21b8e3c16bf5bcb307
Stew Eucen <stew.eucen@gmail.com>
2024-06-21 21:09:40 +0900

    Add test for EnvironmentHandler#isProduction()

--- tests/__tests__/lib/EnvironmentHandler.js
+++ tests/__tests__/lib/EnvironmentHandler.js
@@ -267,3 +267,59 @@ describe('EnvironmentHandler', () => {
     })
   })
 })
+
+describe('EnvironmentHandler', () => {
+  describe('#isProduction()', () => {
+    describe('to be truthy', () => {
+      const cases = [
+        {
+          args: {
+            processEnv: {
+              NODE_ENV: 'production',
+            },
+          },
+        },
+      ]
+
+      test.each(cases)('NODE_ENV: $args.environmentHash.NODE_ENV', ({ args }) => {
+        const handler = EnvironmentHandler.create(args)
+
+        expect(handler.isProduction())
+          .toBeTruthy()
+      })
+    })
+
+    describe('to be falsy', () => {
+      const cases = [
+        {
+          args: {
+            processEnv: {
+              NODE_ENV: 'development',
+            },
+          },
+        },
+        {
+          args: {
+            processEnv: {
+              NODE_ENV: 'extra',
+            },
+          },
+        },
+        {
+          args: {
+            processEnv: {
+              NODE_ENV: 'staging',
+            },
+          },
+        },
+      ]
+
+      test.each(cases)('NODE_ENV: $args.environmentHash.NODE_ENV', ({ args }) => {
+        const handler = EnvironmentHandler.create(args)
+
+        expect(handler.isProduction())
+          .toBeFalsy()
+      })
+    })
+  })
+})
