483f94e0f38466c8f71e035670b93d384f4119db
Stew Eucen <stew.eucen@gmail.com>
2024-06-22 16:04:06 +0900

    Add test for main-exports

---
+++ tests/__tests__/main-exports.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const mainExports = require('../../index')
+
+const DotenvLoader = require('../../lib/DotenvLoader')
+const EnvironmentHandler = require('../../lib/EnvironmentHandler')
+const EnvironmentResolver = require('../../lib/EnvironmentResolver')
+
+describe('main exports', () => {
+  const cases = [
+    {
+      args: {
+        ExportedClass: DotenvLoader,
+      },
+    },
+    {
+      args: {
+        ExportedClass: EnvironmentHandler,
+      },
+    },
+    {
+      args: {
+        ExportedClass: EnvironmentResolver,
+      },
+    }
+  ]
+
+  test.each(cases)('class: $args.ExportedClass.name', ({ args }) => {
+    const targetClass = args.ExportedClass
+
+    expect(mainExports)
+      .toHaveProperty(targetClass.name, targetClass)
+  })
+})
